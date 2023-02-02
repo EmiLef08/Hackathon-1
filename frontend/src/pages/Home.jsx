ef47e4d5a15fe24779f03a883df2e3c84642ab94
EmiLef08 <emilef08@gmail.com>
2023-02-03 00:26:55 +0100

    Nouvelle Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Grid from "../components/Grid";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.home}>
-      <div className={styles.header}>
-        <Header />
+      <div className={styles.titre}>
+        <h1>Bienvenue</h1>
+        <img src="../assets/images/Bienvenue.png" alt="Bienvenue" />
+        <img src="../assets/images/A intérieur du.png" alt="A l'intérieur du" />
+        <img src="../assets/images/TARDIS.png" alt="TARDIS" />
       </div>
-      <h1 className={styles.titre}>Tempus itinerantur</h1>
       <Grid />
       <div>
         <Footer />
