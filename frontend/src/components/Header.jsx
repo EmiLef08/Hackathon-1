644ad7105352693c2140474aaff3842d8df8080a
Xcaliburns <davidabruzzogames@gmail.com>
2023-02-03 14:10:53 +0100

    merci Loris pour les liens

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Anchor, Header as HeaderGrommet, Nav } from "grommet";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
   const [villes, setVilles] = useState([]);
+  const navigate = useNavigate();
   const [lien, setLien] = useState("");
   useEffect(() => {
     fetch("http://localhost:5000/villes")
@@ -14,16 +15,19 @@ function Header() {
 
   const handleSelect = (e) => {
     if (e.target.value === `Tokyo`) {
-      setLien("/Tokyo");
+      navigate("/Tokyo");
     }
     if (e.target.value === `Boston`) {
-      setLien("/Boston");
+      navigate("/Boston");
     }
     if (e.target.value === `Svalbard`) {
-      setLien("/Svalbard");
+      navigate("/Svalbard");
     }
     if (e.target.value === `Quito`) {
-      setLien("/Quito");
+      navigate("/Quito");
+    }
+    if (e.target.value === `Los_Angeles`) {
+      navigate("/Quito");
     }
   };
 
@@ -41,7 +45,7 @@ function Header() {
                 <select onChange={handleSelect}>
                   <option value="">---</option>
                   {villes.map((ville) => (
-                    <option key={ville.id} value={ville.id}>
+                    <option key={ville.id} value={ville.name}>
                       {ville.name}
                     </option>
                   ))}
