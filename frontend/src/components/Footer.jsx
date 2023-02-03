30c8c6be0b7e73c724a5db885a8f674fea262c4a
GessyBoy <ydeletrez@gmail.com>
2023-02-03 02:55:44 +0100

    modif carte Japon

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,34 @@
-import React from "react";
-import { Box, Footer as FooterGrommet, Text } from "grommet";
+import React, { useState } from "react";
+import { Box, Footer as FooterGrommet, Text, Layer } from "grommet";
 
 function Footer() {
+  const [showLayer, setShowLayer] = useState(false);
+
   return (
     <div>
+      {showLayer && (
+        <Layer
+          onEsc={() => setShowLayer(false)}
+          onClickOutside={() => setShowLayer(false)}
+        >
+          <Box pad="medium" gap="small" width="medium">
+            <Text>This is a pop up!</Text>
+          </Box>
+        </Layer>
+      )}
       <Box direction="column" justify="end">
-        <FooterGrommet background="light-4" justify="center" pad="small">
+        <FooterGrommet
+          background="light-4"
+          justify="center"
+          pad="small"
+          onClick={() => setShowLayer(true)}
+        >
           <Text textAlign="center" size="small">
-            © 2019 Copyright Grommet
+            A propos
           </Text>
         </FooterGrommet>
       </Box>
     </div>
   );
 }
-
 export default Footer;
